7d2c80f566b998b0827056a53326417ed1e8003a
Safeya-Yasien <bluesky7yas@gmail.com>
2024-03-29 16:24:18 +0200

    technology city first version

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const landingPage = document.querySelector(".landing");
+const imgs = [
+  "imgs/wallpaperflare-2.jpg",
+  "imgs/wallpaperflare-3.jpg",
+  "imgs/wallpaperflare-1.jpg",
+  "imgs/wallpaperflare-4.jpg",
+  "imgs/wallpaperflare-5.jpg",
+];
+
+let currentIndex = 0;
+
+function changeLandingBackground() {
+  landingPage.style.backgroundImage = `url('${imgs[currentIndex]}')`;
+
+  if (currentIndex == imgs.length - 1) {
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+
+  setTimeout(() => changeLandingBackground(), 3000);
+}
+
+changeLandingBackground();
